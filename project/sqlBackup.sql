6cb0af1b2f97432fa9557893aeb6a620a7fddc14
parin3399 <137426444+parin3399@users.noreply.github.com>
2023-12-19 12:42:48 +0530

    db

---
+++ project/sqlBackup.sql
@@ -0,0 +1,80 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 8.0.31 : Database - project
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `project`;
+
+/*Table structure for table `company` */
+
+DROP TABLE IF EXISTS `company`;
+
+CREATE TABLE `company` (
+  `companyid` int NOT NULL AUTO_INCREMENT,
+  `compname` varchar(50) NOT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`companyid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `company` */
+
+insert  into `company`(`companyid`,`compname`,`category`,`image`) values (1,'Killer','Pents','null'),(2,'Killer','Shirts',NULL),(3,'Polo','Shirts',NULL),(4,'Polo','Tshirts',NULL);
+
+/*Table structure for table `customer` */
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `customerid` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(15) NOT NULL,
+  `lastname` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `mobno` varchar(10) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(25) NOT NULL,
+  `pincode` int NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  PRIMARY KEY (`customerid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `customer` */
+
+insert  into `customer`(`customerid`,`firstname`,`lastname`,`mobno`,`address`,`city`,`pincode`,`email`,`password`) values (1,'Vyomesh','Gauswami','9879243065','Paramba Society','kalol',389330,'goswamivyomesh@gmail.com','Vyom123');
+
+/*Table structure for table `product` */
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `productid` int NOT NULL AUTO_INCREMENT,
+  `companyid` int DEFAULT NULL,
+  `pname` varchar(100) DEFAULT NULL,
+  `size` varchar(7) DEFAULT NULL,
+  `color` varchar(10) DEFAULT NULL,
+  `price` varchar(10) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`productid`),
+  KEY `companyid` (`companyid`),
+  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`companyid`) REFERENCES `company` (`companyid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `product` */
+
+insert  into `product`(`productid`,`companyid`,`pname`,`size`,`color`,`price`,`image`) values (1,4,'PT3453','XXL','Pink','550','123.jpg');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
